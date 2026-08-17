a18543a5469d373bc00e78632c9f9f39c67bc067
nidhigajjar7407 <nidhigajjar7407@gmail.com>
2026-08-17 19:05:16 +0530

    first commit

---
+++ pattern7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+            if(i==1 || i ==3)
+            {
+                for(j=1;j<=5;j++)
+                {
+                    printf("* ");
+                }
+            }
+            else if (i== 2)
+            {
+                printf("*       *");
+            }
+            else{
+                printf("*");
+            }
+            printf("\n");
+        }
+        
+    
+}
